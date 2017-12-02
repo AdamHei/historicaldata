c25c617f61329bae82f4a4b96234796287be4035
Adam Heimendinger <imbrainboy@gmail.com>
2017-12-02 16:00:25 -0600

    Grab the latest timestamp in the db so we don't insert duplicate trades **aMake sure you multiply two int64's when doing large multiplication**

--- populators/gemini.go
+++ populators/gemini.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/adamhei/honorsproject/exchanges/models"
 	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 	"log"
 	"net/http"
 	"time"
@@ -18,26 +19,29 @@ func Populate(db *mgo.Database) {
 	collection := db.C(collectionName)
 	bulkInsert := collection.Bulk()
 
+	latestTimestampMS := getLatestTimestampMs(collection)
+	log.Println("Latest timestampsms is", latestTimestampMS)
 	now := time.Now()
 	count := 0
-	for indexTime := time.Unix(0, int64(firstTradeTimestampMs*time.Millisecond)); indexTime.Before(now); {
+
+	for indexTime := time.Unix(0, latestTimestampMS*int64(time.Millisecond)); indexTime.Before(now); {
 
 		orders := getTradeHistory(indexTime)
+		bulkInsert.Insert(toInterfaceSlice(orders)...)
 
-		bulkInsert.Insert(orders)
-		log.Println("Prepared", len(orders), "orders")
 		// Offset by 1 to exclude last trade
 		indexTime = time.Unix(0, orders[0].TimestampMs*int64(time.Millisecond)+1)
 
 		count++
-		if count % 100 == 0 {
+		// Perform batch after every 100 requests
+		if count%100 == 0 {
 			count = 0
 			res, err := bulkInsert.Run()
 			if err != nil {
 				log.Println("Couldn't perform batch insert")
 				log.Fatal(err)
 			}
-			log.Println("Modified", res.Modified, "records")
+			log.Println("Matched", res.Matched, "docs and modified", res.Modified)
 		}
 	}
 }
@@ -62,4 +66,21 @@ func getTradeHistory(from time.Time) []models.GeminiOrder {
 	time.Sleep(500 * time.Millisecond)
 
 	return orders
+}
+
+func getLatestTimestampMs(coll *mgo.Collection) int64 {
+	res := new(models.GeminiOrder)
+	coll.Find(bson.M{}).Sort("-timestampms").One(&res)
+	if res.TimestampMs != 0 {
+		return res.TimestampMs + 1
+	}
+	return firstTradeTimestampMs
+}
+
+func toInterfaceSlice(orders []models.GeminiOrder) []interface{} {
+	arr := make([]interface{}, len(orders))
+	for i, v := range orders {
+		arr[i] = v
+	}
+	return arr
 }
